61940040d270d0265d1c9e4c3e37213172a0d83a
Will09122000 <will0912@icloud.com>
2020-10-15 14:46:03 +0100

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -5,9 +5,10 @@ public class CardGame {
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter the number of players:");
         int num = scan.nextInt();
+        System.out.print("Please enter location of pack to load:");
+        String fileName = scan.next();
         scan.close();
-        System.out.println("The number entered by user: "+num);
-        //String s = scan.next();
-        //System.out.println(s);
+        System.out.println("The number entered by user: " + num);
+        System.out.println("The file name entered by user: " + fileName);
     }
 }
