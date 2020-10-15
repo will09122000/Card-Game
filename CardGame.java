0395ef40ae9902731695a3b7b1b55491ac41711e
Will09122000 <will0912@icloud.com>
2020-10-15 13:15:27 +0100

    Test

--- CardGame.java
+++ CardGame.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class CardGame {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the number of players:");
+        int num = scan.nextInt();
+        scan.close();
+        System.out.println("The number entered by user: "+num);
+        //String s = scan.next();
+        //System.out.println(s);
+    }
 }
